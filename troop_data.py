a4ee3e8bed377beaeec27afea28e0abb6d6b165d
Amy Lynn <edinger.amy@gmail.com>
2025-04-05 12:56:49 -0400

    Update troop_data.py

--- troop_data.py
+++ troop_data.py
@@ -1,3 +1,29 @@
+# troop_data.py
+
+TROOP_STATS = {
+    # Barbarian Troops
+    'guard10': {'type': 'Barbarian', 'tier': 'T10', 'ATK': 49, 'DEF': 114},
+    'guard7': {'type': 'Barbarian', 'tier': 'T7', 'ATK': 29, 'DEF': 68},
+    'warrior9': {'type': 'Barbarian', 'tier': 'T9', 'ATK': 84, 'DEF': 49},
+    'warrior8': {'type': 'Barbarian', 'tier': 'T8', 'ATK': 70, 'DEF': 41},
+
+    # Rider Troops
+    'rider9': {'type': 'Rider', 'tier': 'T9', 'ATK': 91, 'DEF': 49},
+    'vanguard10': {'type': 'Rider', 'tier': 'T10', 'ATK': 90, 'DEF': 65},
+    'vanguard8': {'type': 'Rider', 'tier': 'T8', 'ATK': 64, 'DEF': 34},
+
+    # Behemoth Troops
+    'marauder9': {'type': 'Behemoth', 'tier': 'T9', 'ATK': 48, 'DEF': 70},
+    'raider10': {'type': 'Behemoth', 'tier': 'T10', 'ATK': 164, 'DEF': 73},
+
+    # Hunter Troops
+    'archer10': {'type': 'Hunter', 'tier': 'T10', 'ATK': 65, 'DEF': 49},
+    'archer8': {'type': 'Hunter', 'tier': 'T8', 'ATK': 47, 'DEF': 35},
+    'javelineer9': {'type': 'Hunter', 'tier': 'T9', 'ATK': 98, 'DEF': 49},
+    'javelineer7': {'type': 'Hunter', 'tier': 'T7', 'ATK': 68, 'DEF': 34}
+}
+
+
 # troop_data.py – All troop types, base stats, and skills
 
 TROOPS = {
